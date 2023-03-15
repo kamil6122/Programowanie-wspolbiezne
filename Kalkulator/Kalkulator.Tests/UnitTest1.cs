66c45195fac0b6e97920dc33f98a616b0a242ae8
Kamil Włodarczyk <242565@edu.p.lodz.pl>
2023-03-15 09:26:47 +0100

    Simple calculator program with unit tests

---
+++ Kalkulator/Kalkulator.Tests/UnitTest1.cs
@@ -0,0 +1,67 @@
+using System.Windows;
+
+namespace Kalkulator.Tests
+{
+
+    public class UnitTest1
+    {
+        [StaTheory]
+        [InlineData(1, 1, 2)]
+        [InlineData(270, 1, 271)]
+        [InlineData(5, 6, 11)]
+        public void AddTest(int x, int y, int expected)
+        {
+            Calculator calculator = new Calculator();
+            int actual = calculator.add(x, y);
+            Assert.Equal(actual, expected);
+        }
+
+        
+        [StaTheory]
+        [InlineData(1, 1, 0)]
+        [InlineData(270, 1, 269)]
+        [InlineData(5, 6, -1)]
+        public void SubstructTest(int x, int y, int expected)
+        {
+            Calculator calculator = new Calculator();
+            int actual = calculator.substruct(x, y);
+            Assert.Equal(actual, expected);
+        }
+
+        [StaTheory]
+        [InlineData(1, 1, 1)]
+        [InlineData(270, 2, 540)]
+        [InlineData(5, -6, -30)]
+        public void MultiplicationTest(int x, int y, int expected)
+        {
+            Calculator calculator = new Calculator();
+            int actual = calculator.multiplication(x, y);
+            Assert.Equal(actual, expected);
+        }
+
+        [StaTheory]
+        [InlineData(1, 1, 1)]
+        [InlineData(270, 2, 135)]
+        [InlineData(5, 6, 0)]
+        public void DivideTest(int x, int y, int expected)
+        {
+            Calculator calculator = new Calculator();
+            int actual = calculator.divide(x, y);
+            Assert.Equal(actual, expected);
+        }
+
+        [StaFact]
+        public void DivideByZeroTest()
+        {
+            Calculator calculator = new Calculator();
+            try
+            {
+                calculator.divide(2, 0);
+
+            } catch(DivideByZeroException ex)
+            {
+                Assert.Equal(0, calculator.divide(2, 0));
+            }
+        }
+    }
+}
---
+++ Kalkulator/Kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Kalkulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        public string getTextFromLiczba_1()
+        {
+            return this.liczba_1.Text;
+        }
+
+        public string getTextFromLiczba_2()
+        {
+            return this.liczba_2.Text;
+        }
+
+        public string getTextFromWynik()
+        {
+            return this.wynik.Text;
+        }
+
+        public int convertStringToInt(string str)
+        {
+            return int.Parse(str);
+        }
+
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int substruct(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int divide(int x, int y)
+        {
+            int result = 0;
+            try
+            {
+                result = x / y;
+
+            } catch(DivideByZeroException ex)
+            {                
+                MessageBox.Show("Nie wolno dzielic przez 0!", ex.Message);
+                
+            }
+            return result;
+        }
+
+        public bool validInput()
+        {
+            Regex regex = new Regex("^[0-9]+$");
+            string liczba1 = this.liczba_1.Text;
+            string liczba2 = this.liczba_2.Text;
+            bool a = regex.IsMatch(liczba1);
+            bool b = regex.IsMatch(liczba2);
+            return regex.IsMatch(this.liczba_1.Text) && regex.IsMatch(this.liczba_2.Text);
+        }
+
+        public bool emptyInput()
+        {
+            return this.liczba_1.Text == "" || this.liczba_2.Text == "";
+        }
+
+        private void onlyNumberAvailable(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void onAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (validInput() && !emptyInput())
+            {
+                this.wynik.Text = add(convertStringToInt(this.liczba_1.Text), convertStringToInt(this.liczba_2.Text)).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Bledne dane!");
+            }
+        }
+
+        private void onSubstruct_Click(object sender, RoutedEventArgs e)
+        {
+            if (validInput() && !emptyInput())
+            {
+                this.wynik.Text = substruct(convertStringToInt(this.liczba_1.Text), convertStringToInt(this.liczba_2.Text)).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Bledne dane!");
+            }
+        }
+
+        private void onMultiple_Click(object sender, RoutedEventArgs e)
+        {
+            if (validInput() && !emptyInput())
+            {
+                this.wynik.Text = multiplication(convertStringToInt(this.liczba_1.Text), convertStringToInt(this.liczba_2.Text)).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Bledne dane!");
+            }
+        }
+
+        private void onDivide_Click(object sender, RoutedEventArgs e)
+        {
+            if (validInput() && !emptyInput())
+            {
+                this.wynik.Text = divide(convertStringToInt(this.liczba_1.Text), convertStringToInt(this.liczba_2.Text)).ToString();
+                if (convertStringToInt(this.liczba_2.Text) == 0)
+                {
+                    this.wynik.Text = "Dzielenie przez 0!";
+                }   
+            }
+            else
+            {
+                MessageBox.Show("Bledne dane!");
+            }
+        }
+
+    }
+}
