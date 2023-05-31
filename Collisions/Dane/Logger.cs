2cb64f2dcae93a16b9e4ccea574517bd7d5c4182
Kamil Włodarczyk <242565@edu.p.lodz.pl>
2023-05-31 13:05:32 +0200

    Changed stopwatch to timer

--- Collisions/Dane/Logger.cs
+++ Collisions/Dane/Logger.cs
@@ -4,47 +4,47 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Timers;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Dane
 {
     internal class Logger
     {
-        private static List<Ball> balls;
-        private Stopwatch stopwatch = new Stopwatch();
+        private List<Ball> balls;
+        private System.Timers.Timer timer;
 
         public Logger(List<Ball> balls) 
         {
-            balls = balls;
-            Thread t = new Thread(() =>
+            this.balls = balls;
+            SetTimer();
+        }
+
+        private void SetTimer()
+        {
+            timer = new System.Timers.Timer(500);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            using (StreamWriter stream = new StreamWriter(Directory.GetCurrentDirectory() + "\\logs.txt", true))
             {
-                stopwatch.Start();
-                while (true)
+                string text = ($"Loggs: {DateTime.Now:R}");
+                foreach (Ball b in this.balls)
                 {
-                    if (stopwatch.ElapsedMilliseconds >= 5)
-                    {
-                        stopwatch.Restart();
-                        using (StreamWriter stream = new StreamWriter(Directory.GetCurrentDirectory() + "\\logs.txt", true))
-                        {
-                            string text = ($"Loggs: {DateTime.Now:R}");
-                            foreach (Ball b in balls)
-                            {
-                                stream.WriteLine(text + JsonSerializer.Serialize(b));
-                            }
-                        }
-                    }
+                    stream.WriteLine(text + JsonSerializer.Serialize(b));
                 }
-            })
-            {
-                IsBackground = true
-            };
-            t.Start();
+            }   
         }
 
         public void stop()
         {
-            stopwatch.Reset();
-            stopwatch.Stop();
+            timer.Stop();
+            timer.Elapsed -= OnTimedEvent;
         }
     }
 }
